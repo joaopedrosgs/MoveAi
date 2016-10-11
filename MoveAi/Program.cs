7b9a33cb0e38c33c2cdde8dbf68e3cdde1d8787c
João Pedro São Gregorio Silva <jsangregorio@hotmail.com>
2016-10-11 09:40:33 -0300

    visual fix

--- MoveAi/Program.cs
+++ MoveAi/Program.cs
@@ -14,8 +14,6 @@ namespace MoveAi
 
     class Program
     {
-
-
         static void Main(string[] args)
         {
             var Ai = new MovementAi();
@@ -28,81 +26,104 @@ namespace MoveAi
                 Console.Write(a);
                 a++;
                 Console.ReadKey();
-
-
             }
         }
     }
-    class MovementAi {
-        public  int[] playerPosition;
-        public  int[] objetivePosition;
-        public  int[,] map;
-        public  List<int[]> walked;
+
+    class MovementAi
+    {
+        public int[] playerPosition;
+        public int[] objetivePosition;
+        public int[,] map;
+        public List<int[]> walked;
 
         public MovementAi()
         {
-
-            playerPosition = new int[2] { 1, 1 };
-            objetivePosition = new int[2] { 9, 8 };
-            walked = new List<int[]>();
-            
+            playerPosition = new int[2] {1, 1};
+            objetivePosition = new int[2] {9, 12};
+            walked = new List<int[]>(10);
 
 
             map = new[,]
             {
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 2 = Entity
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1},
-                {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 1}, // 9 = Objective
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1}, 
+                {1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1},
+                {1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+                {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1},
+                {1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1}, 
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
             };
-
         }
+
         public void PrintMap()
         {
-            for (int y = 0; y < map.GetUpperBound((int) Coord.Y); y++)
+            for (int y = 0; y <= map.GetUpperBound((int) Coord.Y); y++)
             {
-                for (int x = 0; x < map.GetUpperBound((int) Coord.X); x++)
+                for (int x = 0; x <= map.GetUpperBound((int) Coord.X); x++)
                 {
-                    if(x == playerPosition[(int)Coord.X] && y == playerPosition[(int)Coord.Y])
+                    if (x == playerPosition[(int) Coord.X] && y == playerPosition[(int) Coord.Y])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
                         Console.Write("* ");
-                    else
-                        Console.Write(map[y,x] + " ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                 
+                    else if(x == objetivePosition[(int)Coord.X] && y == objetivePosition[(int)Coord.Y])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
 
+                        Console.Write("A ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (map[y, x] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write(map[y, x] + " ");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    }
+                    else
+                    {
+
+                        Console.Write("  ");
+                    }
 
                 }
                 Console.Write("\n");
             }
         }
+
         public void Go()
         {
+            var possible = PossibleMovements();
+            Console.WriteLine($"Atual: {playerPosition[0]}:{playerPosition[1]}");
+            Console.WriteLine($"Y:X - Distancia do alvo");
+
+            foreach (var move in possible)
+            {
+                Console.WriteLine($"{move[0]}:{move[1]} - {Distance(move)}");
+            }
             var tile = Choose(PossibleMovements());
             playerPosition = tile;
 
             walked.Add(tile);
-
         }
 
         public bool CanGo(int[] tile)
         {
-            if (InsideBoundaries(tile) && Walkable(tile))
-                return true;
-            else
-            {
-                Console.WriteLine("erro");
-                return false;
-            }
+   
+                return (InsideBoundaries(tile) && Walkable(tile));
+
         }
 
         public bool InsideBoundaries(int[] tile)
         {
-           
             return tile[(int) Coord.X] <= map.GetUpperBound((int) Coord.X) &&
                    tile[(int) Coord.Y] <= map.GetUpperBound((int) Coord.Y) &&
                    tile[(int) Coord.X] >= 0 &&
@@ -125,10 +146,11 @@ namespace MoveAi
 
         public int[] Choose(List<int[]> tiles)
         {
-
             //return tiles.OrderBy(x => (Distance(x))).FirstOrDefault();
-            return tiles.OrderBy(x => walked.FindAll(entity=>(entity[0] == x[0] && entity[1]==x[1])).Count()).ThenBy(x => (Distance(x))).FirstOrDefault();
-
+            return
+                tiles.OrderBy(x => walked.FindAll(entity => (entity[0] == x[0] && entity[1] == x[1])).Count())
+                    .ThenBy(x => (Distance(x)))
+                    .FirstOrDefault();
         }
 
         public List<int[]> PossibleMovements()
@@ -143,12 +165,12 @@ namespace MoveAi
                         y + playerPosition[(int) Coord.Y],
                         x + playerPosition[(int) Coord.X]
                     };
-    
 
-                    if (CanGo(tile) && (x!=0 || y!=0))
+
+                    if (CanGo(tile) && (x != 0 || y != 0))
                         movements.Add(tile);
                 }
-          
+
             return movements;
         }
     }
